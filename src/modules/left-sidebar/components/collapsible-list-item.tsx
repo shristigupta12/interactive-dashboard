b53d830b31c254ec59752a2307a114c01caa58bf
Shristi Gupta <shristiguptawork@gmail.com>
2025-07-21 02:13:52 +0530

    fix: left sidebar ui

--- src/modules/left-sidebar/components/collapsible-list-item.tsx
+++ src/modules/left-sidebar/components/collapsible-list-item.tsx
@@ -4,12 +4,26 @@ import { useState } from "react";
 import { useNavigate, useLocation } from "react-router-dom"
 import { useTheme } from "../../contexts/theme-context"
 
+export const SubListItem = ({item, theme}:{item:{id:string; name: string}, theme:string}) => {
+    const [hovered, setHovered] = useState(false)
+    return(
+        <div className={`flex gap-2 items-center rounded-[8px] py-1 pr-2 w-full ${theme === 'dark' ? 'hover:bg-white/15' : 'hover:bg-neutral-100'} cursor-pointer`}  onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+            <div className="flex items-center">
+                <div className={`w-1 h-4 rounded-full ${hovered ? theme === 'dark' ? 'bg-white' : 'bg-black' : 'bg-transparent'}`}></div>
+                <div className="size-2"></div>
+            </div>
+            <p className="text-sm truncate w-full min-w-0">{item.name}</p>
+        </div>
+    )
+}
+
 export const CollapsibleListItem = ({list}:{list:ListType}) => {
     const [caretOpen, setCaretOpen] = useState(false);
     const navigate = useNavigate();
     const location = useLocation();
     const currentPath = location.pathname;
     const {theme} = useTheme()
+    const [hovered, setHovered] = useState(false)
     const toggleCaret = () => {
         setCaretOpen(caretOpen => !caretOpen)
     }
@@ -33,14 +47,18 @@ export const CollapsibleListItem = ({list}:{list:ListType}) => {
 
     return(
         <div className="flex flex-col w-full">
-            <div className="flex rounded-2 py-1 pr-2 w-full">
+            <div className={`flex items-center rounded-[8px] py-1 pr-2 w-full ${theme === 'dark' ? 'hover:bg-white/15' : 'hover:bg-neutral-100'} ${isActive() ? theme === 'dark' ? 'bg-white/15' : 'bg-neutral-100' : ''}`} onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+                <div className="flex items-center">
+                    <div className={`w-1 h-4 rounded-full ${isActive() || hovered ? theme === 'dark' ? 'bg-white' : 'bg-black' : 'bg-transparent'}`}></div>
+                    <div className="size-4"></div>
+                </div>
                 {list.subList &&
-                    <div className="pl-1">
-                        <CaretRight size={16}  className={`text-neutral-500 transition-transform duration-500 ${caretOpen ? "rotate-90" : ""}`} onClick={toggleCaret} />
+                    <div className="pl-1 cursor-pointer">
+                        <CaretRight size={14}  className={`text-neutral-400 transition-transform duration-500 ${caretOpen ? "rotate-90" : ""}`} onClick={toggleCaret} />
                     </div>
                 }
                 <div 
-                    className={`flex items-center gap-1 flex-1 cursor-pointer rounded px-1  transition-all duration-200 ${theme === 'dark' ? 'hover:bg-white/15' : 'hover:bg-neutral-100'} ${isActive() ? theme === 'dark' ? 'bg-white/15 border-l border-white border-l-2' : 'bg-neutral-100 border-l border-black border-l-2 ' : ''}`}
+                    className={`flex items-center gap-1 flex-1 cursor-pointer rounded-[8px] px-1  transition-all duration-200`}
                     onClick={handleItemClick}
                 >
                     <list.icon size={16} weight="duotone"  />
@@ -48,11 +66,9 @@ export const CollapsibleListItem = ({list}:{list:ListType}) => {
                 </div>
                 </div>
                 {caretOpen && list.subList && (
-                    <div className="flex flex-col gap-1">
+                    <div className="flex flex-col gap-1 pl-10">
                         {list.subList?.map((item) => (
-                            <div key={item.id} className="flex py-1 pr-2">
-                                <p className="text-sm text-neutral-500">{item.name}</p>
-                            </div>
+                            <SubListItem key={item.id} item={item} theme={theme} />
                         ))}
                     </div>
                 )}
--- src/modules/left-sidebar/components/tabs.tsx
+++ src/modules/left-sidebar/components/tabs.tsx
@@ -1,32 +1,39 @@
 import { useState } from "react";
 import { FavoritesRecent } from "../data/favorites-recent";
+import { useTheme } from "../../contexts/theme-context";
 
-export const TabItem = ({tab}:{tab:string}) => {
+export const TabItem = ({tab, theme}:{tab:string, theme:string}) => {
+    const [hovered, setHovered] = useState(false)
     return(
-        <div className="w-full px-2 py-1 flex gap-2 rounded-2 items-center">
+        <div className={`flex gap-2 items-center rounded-[8px] py-1 pr-2 w-full ${theme === 'dark' ? 'hover:bg-white/15' : 'hover:bg-neutral-100'} cursor-pointer`}  onMouseEnter={() => setHovered(true)} onMouseLeave={() => setHovered(false)}>
+            <div className="flex items-center">
+                <div className={`w-1 h-4 rounded-full ${hovered ? theme === 'dark' ? 'bg-white' : 'bg-black' : 'bg-transparent'}`}></div>
+                <div className="size-4"></div>
+            </div>
             <div className="w-2 h-2 rounded-full bg-neutral-300"></div>
             <p>{tab}</p>
         </div>
     )
 } 
 export const Tabs = () => {
-    const [activeTab, setActiveTab] = useState("favorites");    
+    const [activeTab, setActiveTab] = useState("favorites");  
+    const {theme} = useTheme()
     return(
         <div className="flex flex-col gap-1 pb-3">
-            <div className="flex items-center gap-2">
-                <p className={`cursor-pointer ${activeTab === "favorites" ? "text-neutral-500" : "text-neutral-400"}`} onClick={() => setActiveTab("favorites")}>Favorites</p>
-                <p className={`cursor-pointer ${activeTab === "recently" ? "text-neutral-500" : "text-neutral-400"}`} onClick={() => setActiveTab("recently")}>Recently</p>
+            <div className="flex items-center gap-4">
+                <p className={`cursor-pointer ${activeTab === "favorites" ? theme === 'dark' ? "text-white" : "text-black" : theme === 'dark' ? "text-neutral-400" : "text-neutral-500"}`} onClick={() => setActiveTab("favorites")}>Favorites</p>
+                <p className={`cursor-pointer ${activeTab === "recently" ? theme === 'dark' ? "text-white" : "text-black" : theme === 'dark' ? "text-neutral-400" : "text-neutral-500"}`} onClick={() => setActiveTab("recently")}>Recently</p>
             </div>
             {activeTab==='favorites' ?
                 <div className="flex flex-col gap-1">
                     {FavoritesRecent.favorites.map((item) => (
-                        <TabItem key={item.id} tab={item.name} />
+                        <TabItem key={item.id} tab={item.name} theme={theme} />
                     ))}
                 </div>
             :
                 <div className="flex flex-col gap-1">
                     {FavoritesRecent.recent.map((item) => (
-                        <TabItem key={item.id} tab={item.name} />
+                        <TabItem key={item.id} tab={item.name} theme={theme} />
                     ))}
                 </div>
             }
